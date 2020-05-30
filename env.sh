fc4de36edc85bbef7c818dd7ac3beeb3e20ea3d1
James Jones <atari@theinnocuous.com>
2020-05-30 01:07:05 -0700

    Update env.sh to use actual SDK paths
    Remove unused variables and update the remaining
    ones to point to the correct directories.  Use a
    bash-ism to make the script location-portable.
    Remove the executable bit since env.sh is intended
    to be sourced into the environment rather than
    run.

--- env.sh
+++ env.sh
@@ -1,7 +1,16 @@
-export RDBRC=$HOME/jaguar/bin/rdb.rc
-export DBPATH=$HOME/jaguar/bin
-export ALNPATH=$HOME/jaguar/bin
-export MACPATH=$HOME/jaguar/include
-export PATH=$PATH:$HOME/jaguar/bin
-export SMACPATH=$HOME/jaguar/include
+SCRIPT="${BASH_SOURCE[0]}"
+SCRIPTDIR="`dirname "${SCRIPT}"`"
+SCRIPTDIR="`readlink -f "${SCRIPTDIR}"`"
 
+echo "Setting up Jaguar SDK paths using:"
+echo ""
+echo "  ${SCRIPTDIR}"
+echo ""
+echo "as the base directory"
+
+export RDBRC="${SCRIPTDIR}/jaguar/bin/rdb.rc"
+export DBPATH="${SCRIPTDIR}/jaguar/bin"
+export ALNPATH="${SCRIPTDIR}/jaguar/bin"
+export RMACPATH="${SCRIPTDIR}/jaguar/include"
+export MACPATH="${SCRIPTDIR}/jaguar/include"
+export PATH="${SCRIPTDIR}/jaguar/bin/linux:${SCRIPTDIR}/tools/bin:${PATH}"
