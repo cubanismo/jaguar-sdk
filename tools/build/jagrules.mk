720428e587b7b9ca4bd9970a7cc57d140681a6e6
James Jones <atari@theinnocuous.com>
2025-10-13 22:03:54 -0700

    Add symbol to GPU C files defined to base GPU addr
    This allows programatic loading of separate C
    files to the correct location in GPU memory
    without hard coding each section's address.

--- tools/build/jagrules.mk
+++ tools/build/jagrules.mk
@@ -15,14 +15,24 @@ all: $(PROGS)
 .c.s:
 	$(CC) $(CDEFS) $(CINCLUDES) $(CFLAGS) -S -o $@ $<
 
+# Apply a few patches to the GPU C compiler's assembly output:
+#
+# -Fix the alignment of variables defined in GPU memory
+#
+# -Add a symbol defining the GPU base address of the object file's code.
+#
+# Note these don't handle filenames with illegal symbol characters in them.
+# That's OK, because the similar logic in the GPU C compiler doesn't either!
 $(CGPUOBJS):%o:%c
 	$(CC_JRISC) $(CDEFS) $(CINCLUDES) $(CFLAGS_JRISC) -o $(patsubst %.c,g_%.s,$<) -S $<
 	gawk -i inplace '/.*::?	.DCB.B	8,0/{print "	.long" RS $$0;next}1' $(patsubst %.c,g_%.s,$<)
+	gawk -i inplace '/^gcc2_compiled_for_madmac:/{print $$0 RS "_$(patsubst %.c,%,$<)_loc::";next}1' $(patsubst %.c,g_%.s,$<)
 	$(ASM) $(ASMFLAGS) -u -o $@ $(patsubst %.c,g_%.s,$<)
 
 $(CDSPOBJS):%o:%c
 	$(CC_JRISC) $(CDEFS) $(CINCLUDES) $(CFLAGS_JRISC) $(CFLAGS_DSP) -o $(patsubst %.c,g_%.s,$<) -S $<
 	gawk -i inplace '/.*::?	.DCB.B	8,0/{print "	.long" RS $$0;next}1' $(patsubst %.c,g_%.s,$<)
+	gawk -i inplace '/^gcc2_compiled_for_madmac:/{print $$0 RS "_$(patsubst %.c,%,$<)_loc::";next}1' $(patsubst %.c,g_%.s,$<)
 	$(ASM) $(ASMFLAGS) -u -o $@ $(patsubst %.c,g_%.s,$<)
 
 .PHONY: clean
